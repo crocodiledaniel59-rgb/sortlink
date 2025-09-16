ca180dbcd3f54275e9bfbdee91e1df051c4e84b3
Daniel <crocodiledaniel59@gmail.com>
2025-09-16 20:29:16 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "danielzoldyck-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+// Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch (ambil dari cache dulu, fallback ke network)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate (hapus cache lama kalau ada update)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
